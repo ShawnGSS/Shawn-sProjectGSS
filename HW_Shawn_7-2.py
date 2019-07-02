82790635a2084beb2e64616f8194a7f7c0d7aa54
ShawnGSS <52436415+ShawnGSS@users.noreply.github.com>
2019-07-02 21:45:37 +0800

    Add files via upload

---
+++ HW_Shawn_7-2.py
@@ -0,0 +1,8 @@
+hours = float(input("Input hours here please: "))
+
+htom = int(hours * 60)
+mtos = int(htom * 60)
+
+print(hours , "hours was" , htom , "minutes")
+print("its also" , mtos , "seconds")
+
