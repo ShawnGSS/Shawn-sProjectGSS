95876a9146f7d3199663c04c85d4bd2da273705d
ShawnGSS <52436415+ShawnGSS@users.noreply.github.com>
2019-07-04 21:33:40 +0800

    Create Timestable9x9.java

---
+++ Timestable9x9.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class Klasse74 {
+    public static void main(String[] args) {
+        int TableL = 9;
+
+        for (int x = 1; x <= TableL; x++) {
+
+            for (int y = 1; y <= TableL; y++) {
+                System.out.format("%3d",x * y);
+            }
+            System.out.println("");
+        }
+
+
+    }}
+
