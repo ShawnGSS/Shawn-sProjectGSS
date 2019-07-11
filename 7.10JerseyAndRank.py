63ecb7c12cb12e71832cd1e478e9818e2c96de13
ShawnGSS <52436415+ShawnGSS@users.noreply.github.com>
2019-07-11 11:22:12 +0800

    Update 7.10JerseyAndRank.py

--- 7.10JerseyAndRank.py
+++ 7.10JerseyAndRank.py
@@ -1,14 +1,37 @@
+
+def A(user):
+    if user == 'a':
+        length = 0
+        Jersey = int(input("Enter jersey number: "))
+        Rank = int(input("Enter rank number: "))
+        if (Jersey > 99) or (Jersey < 0) or (Rank > 9) or (Rank < 1):
+            print("Invalid number")
+        else:
+            PlayerDic[Jersey] = Rank
+            length += 1
+def D(user):
+    if user == 'd':
+        Jersey = int(input("Enter player jersey you'd like to delete: "))
+        if Jersey in PlayerDic:
+            del PlayerDic[Jersey]
+        else:
+            print("Player doesnt exist")
+def U(user):
+    if user == 'u':
+        Jersey = int(input("Enter jersey number: "))
+        if Jersey in PlayerDic:
+            PlayerDic[Jersey] = input("Enter new rank number: ")
+        else:
+            print("Player doesnt exist")
+
+    # elif user == 'r':
+    # i dont know how to do R...
+def O(user):
+    if user == 'o':
+        for key in sorted(PlayerDic.keys()):
+            print("Player with the jersey number %s has a rank of %s" % (key, PlayerDic[key]))
 PlayerDic = {}
-length = int(input("Enter player amount: "))
-for i in range(length):
-    Jersey = int(input("Enter jersey number: "))
-    Rank = int(input("Enter rank number: "))
-    if (Jersey > 99)or(Jersey < 0)or(Rank > 9)or(Rank < 1):
-        print("Invalid number")
-    else:
-        PlayerDic[Jersey] = Rank
-for key in sorted(PlayerDic.keys()):
-    print("Player with the jersey number %s has a rank of %s" % (key, PlayerDic[key]))
+
 user = " "
 while user != 'q':
     print("""    a - Add player
@@ -20,32 +43,9 @@ while user != 'q':
     enter your option: 
     """)
     user = input()
-    if user == 'a':
-        Jersey = int(input("Enter jersey number: "))
-        Rank = int(input("Enter rank number: "))
-        if (Jersey > 99) or (Jersey < 0) or (Rank > 9) or (Rank < 1):
-            print("Invalid number")
-        else:
-            PlayerDic[Jersey] = Rank
-            length += 1
-    elif user == 'd':
-        Jersey = int(input("Enter player jersey you'd like to delete: "))
-        if Jersey in PlayerDic:
-            del PlayerDic[Jersey]
-        else:
-            print("Player doesnt exist")
-    elif user == 'u':
-        Jersey = int(input("Enter jersey number: "))
-        if Jersey in PlayerDic:
-            PlayerDic[Jersey] = input("Enter new rank number")
-        else:
-            print("Player doesnt exist")
-   # elif user == 'r':
-        # i dont know how to do R...
-    elif user == 'o':
-        for key in sorted(PlayerDic.keys()):
-            print("Player with the jersey number %s has a rank of %s" % (key, PlayerDic[key]))
+    print(A(user),D(user),U(user),O(user))
 
+print("End of Program.")
 
 
 
