4c9ee7515c28724408b03d9e410294650e96996a
ShawnGSS <52436415+ShawnGSS@users.noreply.github.com>
2019-07-12 09:34:55 +0800

    Create 7.11Squaremachine.java

---
+++ 7.11Squaremachine.java
@@ -0,0 +1,16 @@
+
+public class Squaremachine {
+    private double side;
+    public void setSide(double s) { this.side = s;}
+    double Area(){
+        return side*side;
+    }
+    public static void main(String []args){
+        Scanner input = new Scanner(System.in);
+        Squaremachine side1 = new Squaremachine();
+        System.out.print("Enter square side: ");
+        double si = input.nextDouble();
+        side1.setSide(si);
+        System.out.print(side1.Area());
+    }
+}
