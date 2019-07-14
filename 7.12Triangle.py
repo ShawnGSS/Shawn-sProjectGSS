6f6d47bdc6db6abfa7bcaddde761926e630f118d
ShawnGSS <52436415+ShawnGSS@users.noreply.github.com>
2019-07-14 18:58:22 +0800

    Add files via upload

---
+++ 7.12Triangle.py
@@ -0,0 +1,27 @@
+class Triangle:
+    def side1(self):
+        self.side1
+
+    def side2(self):
+        self.side2
+
+    def side3(self):
+        self.side3
+
+    def __init__(self):
+        self.side1 = 0
+        self.side2 = 0
+        self.side3 = 0
+
+    def read_triangle(self):
+        self.side1 = input("Input side 1: ")
+        self.side2 = input("Input side 2: ")
+        self.side3 = input("Input side 3: ")
+
+    def confirm(self):
+        if self.side1+self.side2>self.side3 and self.side1+self.side3>self.side2 and self.side2+self.side3>self.side1:
+            print("Valid triangle")
+        else:
+            print("Invalid triangle")
+
+
