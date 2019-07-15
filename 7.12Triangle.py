0d0b957453c957dee726c295d5ba3d74676f6de0
ShawnGSS <52436415+ShawnGSS@users.noreply.github.com>
2019-07-15 12:45:12 +0800

    Update 7.12Triangle.py

--- 7.12Triangle.py
+++ 7.12Triangle.py
@@ -1,18 +1,25 @@
 class Triangle:
+
     def __init__(self):
         self.side1 = 0
         self.side2 = 0
         self.side3 = 0
 
     def read_triangle(self):
-        self.side1 = input("Input side 1: ")
-        self.side2 = input("Input side 2: ")
-        self.side3 = input("Input side 3: ")
+        self.side1 = int(input("Input side 1: "))
+        self.side2 = int(input("Input side 2: "))
+        self.side3 = int(input("Input side 3: "))
 
     def confirm(self):
         if self.side1+self.side2>self.side3 and self.side1+self.side3>self.side2 and self.side2+self.side3>self.side1:
             print("Valid triangle")
+            self.perimeter()
         else:
             print("Invalid triangle")
+    def perimeter(self):
+        per = self.side1 + self.side2 + self.side3
+        print(per)
 
-
+T1 = Triangle()
+T1.read_triangle()
+T1.confirm()
