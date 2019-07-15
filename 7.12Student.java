bbcfd6b5f5b160dcdb36b94681100bb0d354f1f7
ShawnGSS <52436415+ShawnGSS@users.noreply.github.com>
2019-07-15 21:31:48 +0800

    Update 7.12Student.java

--- 7.12Student.java
+++ 7.12Student.java
@@ -4,6 +4,12 @@ public class Student {
     private double GPA;
     private String name;
     private int age;
+    private int GraduationDay;
+    private int GraduationMonth;
+    private int GraduationYear;
+    private int GraduationSum;
+
+
     Scanner scnr = new Scanner(System.in);
     ArrayList<Integer> grades = new ArrayList<Integer>();
     //constructor
@@ -29,6 +35,19 @@ public class Student {
         this.name = scnr.next();
         System.out.println("Enter age: ");
         this.age = scnr.nextInt();
+        System.out.println("Enter Graduation Day, Month and Year: ");
+        this.GraduationDay = scnr.nextInt();
+        if(this.GraduationDay > 31){
+            System.out.print("invalid day");
+        }
+        this.GraduationMonth = scnr.nextInt();
+        if(this.GraduationMonth > 12){
+            System.out.print("invalid month");
+        }
+        this.GraduationYear = scnr.nextInt();
+        if(this.GraduationYear > 2017){
+            System.out.print("invalid year");
+        }
         System.out.println("Enter how many grades you'd like to input:  ");
         int length = scnr.nextInt();
         System.out.println("Enter the grades now: ");
@@ -47,13 +66,36 @@ public class Student {
         System.out.println("Student grades: "+ this.grades);
         System.out.println("Student GPA: "+ this.GPA);
     }
+    public int CompareStudents(Student o){
+        this.GraduationSum = this.GraduationDay + this.GraduationMonth * 30 + this.GraduationYear * 365;
+        int Osum = o.getGraduationDay() + o.getGraduationMonth() * 30 + o.getGraduationYear() * 365;
+        if(this.GraduationSum == Osum){
+            return 0;
+        }else if (this.GraduationSum < Osum){
+            return -1;
+        }else{
+            return 1;
+        }
+
+
+    }
+
 
 //setter and getter
     public void setGPA(double GPA) {this.GPA = GPA;}
     public void setAge(int age) {this.age = age;}
     public void setName(String name) {this.name = name;}
+    public void setGraduationDay(int GraduationDay) {this.GraduationDay = GraduationDay;}
+    public void setGraduationMonth(int GraduationMonth) {this.GraduationMonth = GraduationMonth;}
+    public void setGraduationYear(int GraduationYear) {this.GraduationYear = GraduationYear;}
+    public void setGraduationSum(int GraduationSum) {this.GraduationSum = GraduationSum;}
 
     public double getGPA() { return GPA; }
     public double getAge() { return age; }
     public String getName() { return name; }
+    public int getGraduationDay() { return GraduationDay; }
+    public int getGraduationMonth() { return GraduationMonth; }
+    public int getGraduationYear() { return GraduationYear; }
+    public int getGraduationSum() { return GraduationSum; }
 }
+
