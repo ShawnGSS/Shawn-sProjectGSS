fb35f491bba93de7500bf47a3b8e94804b51c48f
ShawnGSS <52436415+ShawnGSS@users.noreply.github.com>
2019-07-22 09:30:10 +0800

    Update 7.19SquareTester.java

--- 7.19SquareTester.java
+++ 7.19SquareTester.java
@@ -2,7 +2,7 @@ public class SquareTester {
     public static void main(String []args){
         Square square = new Square(14);
         int x = square.getLength();
-        System.out.println(square.toString(x));
+        System.out.println(square.toString(x,x));
         System.out.println(" ith a perimeter of: "+ (x + x) + " and an area of: " + x*x);
 
     }
