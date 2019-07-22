a0eb25a74e8f248ae840c86a2606783dc5f0d427
ShawnGSS <52436415+ShawnGSS@users.noreply.github.com>
2019-07-22 09:29:04 +0800

    Update 7.19Square.java

--- 7.19Square.java
+++ 7.19Square.java
@@ -1,10 +1,11 @@
 public class Square extends Rectangle{
 
-    //Square(int length){
-    //  super.getLength() = length;
-    //}
-
-    public String toString(int length){
+    Square(int x){
+      setLength(x);
+      setWidth(x);
+    }
+    @Override
+    public String toString(int length, int width){
         return "this is a square, with a side length of; "+ length;
     }
 }
