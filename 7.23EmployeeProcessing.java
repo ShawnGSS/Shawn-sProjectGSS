6d69340c0cf541f45f0426e0760439a575687b09
ShawnGSS <52436415+ShawnGSS@users.noreply.github.com>
2019-07-23 21:00:31 +0800

    Update 7.23EmployeeProcessing.java

--- 7.23EmployeeProcessing.java
+++ 7.23EmployeeProcessing.java
@@ -47,7 +47,6 @@ public class EmployeeProcessing {
         }
         outFS.println("Total company spending: "+ totalspending);
         outFS.flush();
-        findEmp(pupil,"Anderson");
 
     }
   /*  public static void findEmp(ArrayList<Employee> pupil, String lastname) {
