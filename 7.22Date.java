33ba61dc67d64e59909ad809511e92a5b3b5c3cc
ShawnGSS <52436415+ShawnGSS@users.noreply.github.com>
2019-07-23 20:03:02 +0800

    Update 7.22Date.java

--- 7.22Date.java
+++ 7.22Date.java
@@ -2,15 +2,18 @@ public class Date {
     private int day;
     private int month;
     private int year;
+
     public void setDay(int day){this.day = day; }
     public void setMonth(int month){this.month = month; }
     public void setYear(int year){this.year = year; }
+
     public int getDay(){return day; }
     public int getMonth(){return month; }
     public int getYear(){return year; }
 
-    public String toString(int day, int month, int year){
-        return day + "/" + month + "/" + year;
+
+    public String toString(){
+        return this.day + "/" + this.month + "/" + this.year;
     }
 
     public boolean equals(Date o){
