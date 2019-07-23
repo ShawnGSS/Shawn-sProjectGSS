707140ff1a9aa911edabad623d19608b3080fb20
ShawnGSS <52436415+ShawnGSS@users.noreply.github.com>
2019-07-23 20:02:34 +0800

    Update 7.22Employee.java

--- 7.22Employee.java
+++ 7.22Employee.java
@@ -1,10 +1,5 @@
-
-
-
-// ATTENTION , This file is still buggy, I will have to ask the professor for help 
-// How do I make EmploymentDate type Date? 
-
-
+import java.io.IOException;
+import java.io.PrintWriter;
 
 public class Employee {
     private String firstName;
@@ -17,8 +12,8 @@ public class Employee {
     public void setFirstName(String firstName){this.firstName = firstName; }
     public void setLastName(String lastName){this.lastName = lastName; }
     public void setEID(int eID){this.eID = eID; }
-    public void setBirthDate(Date birthDate){this.birthDate = birthDate; }
-    public void setEmploymentDate(Date employmentDate){this.employmentDate = employmentDate; }
+    public void setBirthDate(Date date){this.birthDate = date; }
+    public void setEmploymentDate(Date date){this.employmentDate = date; }
     public void setSalaryPerDay(int salaryPerDay){this.salaryPerDay = salaryPerDay; }
 
     public String getFirstName(){return firstName; }
@@ -28,10 +23,21 @@ public class Employee {
     public Date getEmploymentDate(){return employmentDate; }
     public int getSalaryPerDay(){return salaryPerDay ; }
 
+
+
+
     public String toString(Employee e){
         return String.format("Employee: %s %s \n ID: %s \nBirth date: %s Employment date %s\nSalary per day: %s", e.getFirstName(), e.getLastName(), e.getEID(), e.getBirthDate(), e.getEmploymentDate(), e.getSalaryPerDay());
     }
-    public int eMoney(Employee e){
-        return getEmploymentDate() * getSalaryPerDay();
+    public int eMoney(){
+        int employmentTime = (2019 * 365 + 7*30 + 23) - (employmentDate.getDay() + employmentDate.getMonth() * 30+employmentDate.getYear() * 365);
+        return employmentTime * getSalaryPerDay();
     }
+    public void printer(PrintWriter outFS)throws IOException {
+        outFS.println("Employee "+ this.firstName + " " + this.lastName);
+        outFS.format("ID: %s, received payment so far: %s", this.eID, this.eMoney());
+        outFS.println(" ");
+        outFS.flush();
+    }
+
 }
