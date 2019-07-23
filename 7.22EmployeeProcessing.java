7f3585f8b26d9e0a9bf72280bca5b344f3a31f5d
ShawnGSS <52436415+ShawnGSS@users.noreply.github.com>
2019-07-23 20:01:59 +0800

    Update 7.22EmployeeProcessing.java

--- 7.22EmployeeProcessing.java
+++ 7.22EmployeeProcessing.java
@@ -1,8 +1,3 @@
-//ATTENTION , WOULD NOT WORK, NEED TO ASK PROFESSOR
-
-
-
-
 import java.io.FileOutputStream;
 import java.io.PrintWriter;
 import java.util.ArrayList;
@@ -15,32 +10,47 @@ public class EmployeeProcessing {
         //Input file to processor
         FileInputStream fileByteStream = null;
         Scanner inFS = null;
-        fileByteStream = new FileInputStream("C:\\Users\\123\\IdeaProjects\\GSS 7-3\\src\\student_file.txt");
+        fileByteStream = new FileInputStream("C:\\Users\\123\\IdeaProjects\\GSS 7-3\\src\\input.txt");
         inFS = new Scanner(fileByteStream);
-
-        //Output processed info to new file
-        FileOutputStream filebytestream = null;
-        PrintWriter outFS = null;
-        filebytestream = new FileOutputStream("C:\\Users\\123\\IdeaProjects\\GSS 7-3\\src\\student_file_printed.txt");
-        outFS = new PrintWriter(filebytestream);
 
         //set values
         ArrayList<Employee> pupil = new ArrayList<Employee>();
         int t = inFS.nextInt();
         for (int p = 0; p < t; p++) {
             Employee emp = new Employee();
+            Date ed = new Date();
+            Date bd = new Date();
             pupil.add(emp);
             emp.setFirstName(inFS.next());
             emp.setLastName(inFS.next());
             emp.setEID(inFS.nextInt());
-            emp.setBirthDate(inFS.nextDate());
-            emp.setEmploymentDate(inFS.nextInt());
-
-            outFS.println("Employee "+ emp.getFirstName() + " " + emp.getLastName());
-            outFS.format("ID: %s, received payment so far: %s", emp.getEID(), emp.get);
-
-            outFS.flush();
+            //Employment date
+            ed.setDay(inFS.nextInt());
+            ed.setMonth(inFS.nextInt());
+            ed.setYear(inFS.nextInt());
+            emp.setEmploymentDate(ed);
+            //Birth date
+            bd.setDay(inFS.nextInt());
+            bd.setMonth(inFS.nextInt());
+            bd.setYear(inFS.nextInt());
+            emp.setBirthDate(bd);
+            emp.setSalaryPerDay(inFS.nextInt());
         }
+        FileOutputStream filebytestream = null;
+        PrintWriter outFS = null;
+        filebytestream = new FileOutputStream("C:\\Users\\123\\IdeaProjects\\GSS 7-3\\src\\output.txt");
+        outFS = new PrintWriter(filebytestream);
+        int totalspending = 0;
+        for (int i = 0; i < t; i++) {
+            pupil.get(i).printer(outFS);
+            totalspending = totalspending + pupil.get(i).eMoney();
+        }
+        outFS.println("Total company spending: "+ totalspending);
+        outFS.flush();
+
+
+        
 
     }
 }
+
